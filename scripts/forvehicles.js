72ba48e083e431e6080a1fe235f8462c1f3eddd9
Muhammad Abdurrauf <muh.nur.abdurrauf@gmail.com>
2021-07-21 23:25:49 +0700

    add new assets, add scroll(unfinished), add level 2,3,4(unfinished)

--- scripts/forvehicles.js
+++ scripts/forvehicles.js
@@ -1,20 +1,22 @@
 import vehicles from "./vehiclesData.js";
+import { randomInt } from './commonFunctions.js';
 
 function checkTurn(vehicle, turnCheck) {
 	const turnArray = turnCheck.instVars.turnAngle
 		.split('|').map(str => str.split(','));
 
 	const curAngle = vehicle.instVars.currentAngle;
+	const melawanArus = vehicle.instVars.pelanggaran == 1? 1 : 0;
 
-	// 	console.log(curAngle);
+		console.log(curAngle, melawanArus);
 
-	const turnable = turnArray.filter(val => val[0] == curAngle);
+	const turnable = turnArray.filter(val => val[0] == melawanArus && val[1] == curAngle);
 
 	const rand = Math.floor(Math.random() * turnArray.length);
 
-	const [, target, targetX, targetY] = turnable[rand];
+	const [, , target, targetX, targetY] = turnable[rand];
 
-	// 	console.table(target, targetX, targetY);
+		console.table(target, targetX, targetY);
 
 	if (target != curAngle) {
 		vehicle.instVars.targetAngle = target;
@@ -24,6 +26,23 @@ function checkTurn(vehicle, turnCheck) {
 	}
 }
 
+function pickVehicle(pelanggaran) {
+	let pickedVehicle = "";
+
+	if(pelanggaran == 2) {
+		pickedVehicle = "motorbikeWithoutHelm";
+	} else if (pelanggaran == 3) {
+		pickedVehicle = "motorbikeWithoutLight";
+	} else {
+		const vehiclesArray = Object.keys(vehicles).filter(v => v !== "motorbikeWithoutHelm" 
+														&& v !== "motorbikeWithoutLight");
+
+		pickedVehicle = vehiclesArray[randomInt(0, vehiclesArray.length - 1)]
+	}
+
+	return pickedVehicle;
+}
+
 function pickType(vehicle) {
-	return Math.floor(Math.random() * vehicles[vehicle]) + 1;
+	return randomInt(1, vehicles[vehicle]);
 }
