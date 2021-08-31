5ff8c06ed6e6151f04657a0e5bf89ddba0e2d0ec
Muhammad Abdurrauf <muh.nur.abdurrauf@gmail.com>
2021-08-31 22:16:13 +0700

    change deadlock detection

--- scripts/forvehicles.js
+++ scripts/forvehicles.js
@@ -24,12 +24,68 @@ function checkTurn(vehicle, turnCheck) {
 
 // 	console.table({targetAngle, targetX, targetY});
 
-	if (targetAngle != curAngle) {
-		vehicle.instVars.targetAngle = targetAngle;
-		vehicle.instVars.targetTurnX = targetX;
-		vehicle.instVars.targetTurnY = targetY;
-		vehicle.instVars.turningId = turnCheck.instVars.id;
+	vehicle.instVars.targetAngle = targetAngle;
+	vehicle.instVars.targetTurnX = targetX;
+	vehicle.instVars.targetTurnY = targetY;
+	vehicle.instVars.turningId = turnCheck.instVars.intersectionId;
+}
+
+function checkLock(vehicle, lockCheck) {
+// 	console.log("vehicle uid " + vehicle.uid);
+	
+	const turnArray = lockCheck.instVars.lockList
+		.split('|').map((str) => str.split(',').map(num => parseInt(num)));
+	
+// 	console.log(turnArray);
+	
+	const curAngle = vehicle.instVars.currentAngle;
+	const targetAngle = vehicle.instVars.targetAngle;
+	const targetX = vehicle.instVars.targetTurnX;
+	const targetY = vehicle.instVars.targetTurnY;
+	const pelanggaran = vehicle.instVars.pelanggaran == 1 
+						|| vehicle.instVars.pelanggaran == 5 
+						? vehicle.instVars.pelanggaran : 0;
+	
+// 	console.log(vehicle);
+// 	console.table({curAngle, targetAngle, targetX, targetY, pelanggaran});
+
+	const turn = turnArray.find(val => val[0] == pelanggaran 
+								  && val[1] == curAngle
+								  && val[2] == targetAngle
+								  && val[3] == targetX
+								  && val[4] == targetY);
+
+// 	console.log(turn);
+	if(turn) {
+		const intersectionLock = [lockCheck.instVars.intersectionId, turn[5], turn[6], turn[7]]
+		vehicle.instVars.intersectionLock = intersectionLock.toString();
+		
+		return intersectionLock;
 	}
+}
+
+function lockIntersection(vehicle, allIntersections) {
+	const pickedLock = vehicle.instVars.intersectionLock.split(",").map(val => parseInt(val));
+	console.log(pickedLock);
+	
+	const pickedIntersections = [];
+	
+	for(let i=1;i<=3;i++){
+		if (pickedLock[i] == -1) continue;
+		
+		const intersection = allIntersections.find(val => val.instVars.intersectionId == pickedLock[0]
+													  && val.instVars.id == pickedLock[i]
+													  && val.instVars.vehiclesUID == "");
+		if(!intersection) {
+			return false;
+		} else {
+			pickedIntersections.push(intersection);
+		}
+	}
+	
+	pickedIntersections.forEach(val => val.instVars.vehiclesUID = vehicle.uid);
+	
+	return true;
 }
 
 function pickVehicle(pelanggaran) {
